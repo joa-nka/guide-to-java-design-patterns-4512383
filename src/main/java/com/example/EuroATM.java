3a973f003a00b85bf32df923e8369293781f93df
joa-nka <jooanka93@gmail.com>
2026-01-13 13:49:09 +0100

    changes

--- src/main/java/com/example/EuroATM.java
+++ src/main/java/com/example/EuroATM.java
@@ -2,8 +2,17 @@ package com.example;
 
 public class EuroATM extends ATM {
 
+    public EuroATM(ATM nextCurrency) {
+        super(nextCurrency);
+    }
+
     @Override
     public void dispense(WithdrawalRequest request) {
-        System.out.println("Dispensing €" + request.getAmount());
+        if (request.getCurrency() == WithdrawalRequest.Currency.EUR) {
+            System.out.println("Dispensing €" + request.getAmount());
+        } else if (nextCurrency != null) {
+            nextCurrency.dispense(request);
+        }
+
     }
 }
--- src/main/java/com/example/UsDollarATM.java
+++ src/main/java/com/example/UsDollarATM.java
@@ -2,9 +2,17 @@ package com.example;
 
 public class UsDollarATM extends ATM {
 
+    public UsDollarATM(ATM nextCurrency) {
+        super(nextCurrency);
+    }
+
     @Override
     public void dispense(WithdrawalRequest request) {
-        System.out.println("Dispensing $" + request.getAmount());
+        if (request.getCurrency() == WithdrawalRequest.Currency.USD) {
+            System.out.println("Dispensing $" + request.getAmount());
+        } else if (nextCurrency != null) {
+            nextCurrency.dispense(request);
+        }
     }
 
 
